ba0dc7372ebf609f880cca2cd556931b396441e4
anAI <anai@anAIs-Air.lan>
2026-04-13 13:39:15 +0530

    updated new domain anai.blog

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/usaco-classes',
-        destination: 'https://anai.live/',
+        destination: 'https://anai.blog/',
         permanent: false,
       }
     ]
